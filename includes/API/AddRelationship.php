a1027452b82c2c5275762dc4376d783072ec52f5
Anton Vanyukov <a.vanyukov@vcore.ru>
2022-09-15 14:54:00 +1000

    Improve post type detection and fix posts not creating when title is empty

--- includes/API/AddRelationship.php
+++ includes/API/AddRelationship.php
@@ -48,8 +48,25 @@ class AddRelationship extends API {
 			return array();
 		}
 
+		$target_post_type = $request->get_param( 'post_type' );
+
+		if ( $object_type === 'post' ) {
+			$found_post_type = false;
+			foreach ( (array) $target_post_type as $post_type ) {
+				if ( post_type_exists( $post_type ) ) {
+					$found_post_type = $post_type;
+					break;
+				}
+			}
+
+			if ( ! $found_post_type ) {
+				return array();
+			}
+
+			$target_post_type = $found_post_type;
+		}
+
 		$current_post_id   = (int) $request->get_param( 'current_post_id' );
-		$target_post_type  = sanitize_text_field( $request->get_param( 'post_type' ) );
 		$relationship_name = sanitize_text_field( $request->get_param( 'relationship_name' ) );
 
 		$results = array();
@@ -79,6 +96,7 @@ class AddRelationship extends API {
 	private function create_post( int $c_post_id, string $t_post_type, string $relationship_name ): array {
 		$new_post_id = wp_insert_post(
 			array(
+				'post_title'  => esc_html__( 'Draft post', 'tenup-content-connect' ),
 				'post_status' => 'draft',
 				'post_type'   => $t_post_type,
 			)
@@ -93,7 +111,7 @@ class AddRelationship extends API {
 
 		return array(
 			'ID'    => $new_post_id,
-			'name'  => 'Draft post',
+			'name'  => esc_html__( 'Draft post', 'tenup-content-connect' ),
 			'added' => true,
 		);
 	}
